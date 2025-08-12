d175d5f6430c8d8d56a2a5555bd9d3272cc7a888
shinjuhyeon <wngus4595@gmail.com>
2025-08-12 17:38:54 +0900

    feat: import문 수정

--- backend/domain/AI/openAI/FeedbackLLMRepository.ts
+++ backend/domain/AI/openAI/FeedbackLLMRepository.ts
@@ -1,5 +1,5 @@
 import { RequestFeedbackDto } from '@/backend/application/feedbacks/dtos/RequestFeedbackDto';
-import { Feedback } from '../entities/Feedback';
+import { Feedback } from '../../entities/Feedback';
 
 export interface FeedbackLLMRepository {
   generateFeedback(requestFeedbackDto: RequestFeedbackDto): Promise<Feedback>;
