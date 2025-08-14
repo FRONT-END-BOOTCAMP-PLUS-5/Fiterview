1fbf17f080e0ac725f5fdd5909a156db13d234ef
김지나 <jina4066@naver.com>
2025-08-14 11:23:16 +0900

    feat: 질문별 오디오 파일 가져오는 메서드 추가

--- backend/domain/repositories/QuestionRepository.ts
+++ backend/domain/repositories/QuestionRepository.ts
@@ -1,12 +1,11 @@
-import { Question } from '@/backend/domain/entities/Question';
+import { Question, AudioFileInfo } from '@/backend/domain/entities/Question';
+
 export interface QuestionRepository {
   // questions테이블의 질문만 조회(TTS 요청 시 사용)
   getQuestionsByReportId(reportId: number): Promise<Question[]>;
   // questions테이블 조회(report 조회 시 사용)
   findAllByReportId(reportId: number): Promise<Question[]>;
 
-  // questions테이블의 사용자 답변만 조회(STT 요청 시 사용)
-  // 사용자 답변 수정
-  // getUserAnswer():
-  // updateUserAnswer():
+  // audio 관련 메서드
+  getAudioFileByQuestion(reportId: number, questionOrder: number): Promise<AudioFileInfo>;
 }
