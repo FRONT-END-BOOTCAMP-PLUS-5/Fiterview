e9a0f6008b9065d88770d57cb6ccb19ebc0943fa
shinjuhyeon <wngus4595@gmail.com>
2025-08-16 02:29:38 +0900

    feat: 인터뷰 페이지 상단 영역 구현

--- app/(anon)/interview/components/TopSection.tsx
+++ app/(anon)/interview/components/TopSection.tsx
@@ -1,6 +1,7 @@
 'use client';
 
 import Timer from './Timer';
+import WaveAnimation from '@/public/assets/icons/wave-animation.svg';
 
 export default function TopSection({
   seconds,
@@ -12,6 +13,11 @@ export default function TopSection({
   return (
     <section className="flex h-[70px] px-[32px] flex-col justify-center items-center gap-[16px] border-b border-slate-200 bg-[#F8FAFC]">
       <Timer seconds={seconds} totalSeconds={totalSeconds} />
+      <div className="flex items-center gap-[8px]">
+        <span className="flex w-[10px] h-[10px] bg-[#3B82F6] rounded-[5px]"></span>
+        <p className="text-[#3B82F6] text-[13px] ">녹음 중</p>
+        <WaveAnimation fill="#3B82F6" />
+      </div>
     </section>
   );
 }
