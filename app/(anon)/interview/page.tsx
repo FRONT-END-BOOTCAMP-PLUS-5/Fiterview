ad95fc01dc071158f8e04fc5717051b71d22756e
shinjuhyeon <wngus4595@gmail.com>
2025-08-16 18:28:20 +0900

    feat: 인터뷰 페이지 하단 영역 구현

--- app/(anon)/interview/page.tsx
+++ app/(anon)/interview/page.tsx
@@ -2,19 +2,22 @@
 
 import { useEffect, useState } from 'react';
 import TopSection from './components/TopSection';
+import BottomSection from './components/BottomSection';
 
 export default function Page() {
   const [seconds, setSeconds] = useState(60);
 
   useEffect(() => {
     const interval = setInterval(() => {
-      setSeconds((prev) => prev - 1);
+      setSeconds((prev) => Math.max(prev - 1, 0));
     }, 1000);
     return () => clearInterval(interval);
   }, []);
   return (
-    <>
+    <div className="h-screen overflow-hidden flex flex-col">
       <TopSection seconds={seconds} totalSeconds={60} />
-    </>
+      <main className="flex-1">{/* 콘텐츠 영역 (스크롤 없음) */}</main>
+      <BottomSection current={1} total={10} onNext={() => {}} />
+    </div>
   );
 }
