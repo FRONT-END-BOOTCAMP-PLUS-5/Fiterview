40505c50a8934cbace065740def9cf9ca46e5938
David Song <david.jh.song@gmail.com>
2025-08-18 17:16:26 +0900

    fix: build error fix

--- app/(anon)/reports/[id]/page.tsx
+++ app/(anon)/reports/[id]/page.tsx
@@ -1,9 +1,11 @@
 import AIFeedback from './components/AIFeedback';
 
-export default function Page({ params }: { params: { id: string } }) {
+export default async function Page({ params }: { params: Promise<{ id: string }> }) {
+  const { id } = await params;
+
   return (
     <div>
-      <AIFeedback reportId={parseInt(params.id, 10)} />
+      <AIFeedback reportId={parseInt(id, 10)} />
     </div>
   );
 }
