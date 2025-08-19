c0b3e09a0040649c7ebd38098a5f8cc9ac126848
gn-ioeo <thdrkdms1030@hanyang.ac.kr>
2025-08-19 17:48:27 +0900

    feat: avatar 연결중 (progress)

--- app/(anon)/interview/[id]/components/AiAvatar.tsx
+++ app/(anon)/interview/[id]/components/AiAvatar.tsx
@@ -1,11 +1,52 @@
 'use client';
 
-import Character from '@/public/assets/icons/character.svg';
+import Avatar from './avatar/Avatar';
+import { useRef, useEffect, useState, Suspense } from 'react';
+import { Canvas } from '@react-three/fiber';
 
-export default function AiAvatar() {
+export default function AiAvatar({
+  ttsAudio,
+  playing,
+}: {
+  ttsAudio: HTMLAudioElement | null;
+  playing: boolean;
+}) {
+  // TTS 오디오 분석용
+  const analyser = useRef<AnalyserNode | null>(null);
+  const timeBuf = useRef<Uint8Array | null>(null);
+  const [energy, setEnergy] = useState(0);
+
+  useEffect(() => {
+    if (!ttsAudio) return;
+    const ctx = new (window.AudioContext || (window as any).webkitAudioContext)();
+    const source = ctx.createMediaElementSource(ttsAudio);
+    analyser.current = ctx.createAnalyser();
+    analyser.current.fftSize = 2048;
+    timeBuf.current = new Uint8Array(analyser.current.fftSize);
+    source.connect(analyser.current);
+    analyser.current.connect(ctx.destination);
+    return () => {
+      source.disconnect();
+      analyser.current?.disconnect();
+      ctx.close();
+    };
+  }, [ttsAudio]);
+
   return (
-    <div className={`flex h-[400px] items-center justify-center`}>
-      <Character width={140} height={90} strokeWidth={0} />
+    <div className="flex h-[500px] w-full items-center justify-center">
+      <Canvas className="h-full w-full" camera={{ position: [0, 0, 0], fov: 10 }}>
+        {/* <ambientLight intensity={0.7} />
+        <directionalLight position={[3, 3, 3]} intensity={1} /> */}
+        <Suspense fallback={null}>
+          <Avatar
+            url="https://models.readyplayer.me/689d3904c911aabc2eba50a6.glb"
+            analyser={analyser}
+            timeBuf={timeBuf}
+            playing={playing}
+            onEnergy={setEnergy}
+          />
+        </Suspense>
+      </Canvas>
     </div>
   );
 }
