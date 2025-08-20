4bcd3b4db6e336c144b401aa5085d1526d24a3bc
김지나 <jina4066@naver.com>
2025-08-20 12:11:25 +0900

    feat: 로그아웃 커스텀 훅 추가

--- app/(anon)/components/modal/LogoutModal.tsx
+++ app/(anon)/components/modal/LogoutModal.tsx
@@ -1,25 +1,20 @@
 'use client';
-import { useRouter } from 'next/navigation';
 import ModalOverlay from './ModalOverlay';
 import Modal from './Modal';
 import { useModalStore } from '@/stores/useModalStore';
+import { useLogout } from '@/hooks/useLogout';
 
 export default function LogoutModal() {
   const { isOpen, closeModal } = useModalStore();
-  const router = useRouter();
+  const { handleLogout } = useLogout();
 
   const ModalButton = () => {
     return (
-      <button className="self-stretch h-11 bg-[#3B82F6] rounded-lg inline-flex justify-center items-center">
-        <div
-          className="text-white text-sm font-semibold cursor-pointer"
-          onClick={() => {
-            closeModal();
-            router.push('/login');
-          }}
-        >
-          로그아웃하기
-        </div>
+      <button
+        className="self-stretch h-11 bg-[#3B82F6] rounded-lg inline-flex justify-center items-center"
+        onClick={handleLogout}
+      >
+        <div className="text-white text-sm font-semibold cursor-pointer">로그아웃하기</div>
       </button>
     );
   };
