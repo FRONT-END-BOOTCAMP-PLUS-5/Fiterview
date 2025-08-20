bbe92c3c8884674c8583505c8f612954eecdd8d3
David Song <david.jh.song@gmail.com>
2025-08-20 17:09:47 +0900

    fix: merge err fixed

--- app/(anon)/components/modal/LogoutModal.tsx
+++ app/(anon)/components/modal/LogoutModal.tsx
@@ -1,20 +1,27 @@
 'use client';
+import { useRouter } from 'next/navigation';
+import { signOut } from 'next-auth/react';
 import ModalOverlay from './ModalOverlay';
 import Modal from './Modal';
 import { useModalStore } from '@/stores/useModalStore';
-import { useLogout } from '@/hooks/useLogout';
 
 export default function LogoutModal() {
   const { isOpen, closeModal } = useModalStore();
-  const { handleLogout } = useLogout();
+  const router = useRouter();
 
   const ModalButton = () => {
     return (
-      <button
-        className="self-stretch h-11 bg-[#3B82F6] rounded-lg inline-flex justify-center items-center"
-        onClick={handleLogout}
-      >
-        <div className="text-white text-sm font-semibold cursor-pointer">로그아웃하기</div>
+      <button className="self-stretch h-11 bg-[#3B82F6] rounded-lg inline-flex justify-center items-center">
+        <div
+          className="text-white text-sm font-semibold cursor-pointer"
+          onClick={async () => {
+            await signOut({ redirect: false });
+            closeModal();
+            router.push('/login');
+          }}
+        >
+          로그아웃하기
+        </div>
       </button>
     );
   };
