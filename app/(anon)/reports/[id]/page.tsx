a46fbd54fd43f5c038cd51d609efc717365f4ee4
David Song <david.jh.song@gmail.com>
2025-08-20 16:31:03 +0900

    feat: reports page.tsx edit

--- app/(anon)/reports/[id]/page.tsx
+++ app/(anon)/reports/[id]/page.tsx
@@ -4,14 +4,13 @@ import Reflection from './components/Reflection';
 
 export default async function ReportPage({ params }: { params: Promise<{ id: string }> }) {
   const { id } = await params;
-  return <AudioReportViewer reportId={id} />;
-  //   <AIFeedback reportId={parseInt(id, 10)} />
 
   const reflection = '';
 
   return (
     <div>
-      {/* <AIFeedback reportId={parseInt(id, 10)} /> */}
+      <AIFeedback reportId={parseInt(id, 10)} />
+      <AudioReportViewer reportId={id} />
       <Reflection reflection={reflection} />
     </div>
   );
