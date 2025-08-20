ba44d72366094cddc99786a72f670385223f6551
gn-ioeo <thdrkdms1030@hanyang.ac.kr>
2025-08-21 00:56:52 +0900

    style: 드롭다운 css 수정

--- app/(anon)/components/user/DropDown.tsx
+++ app/(anon)/components/user/DropDown.tsx
@@ -8,26 +8,26 @@ export default function DropDown() {
   const { openModal } = useModalStore();
 
   return (
-    <nav className="w-[150px] absolute right-0 top-8 bg-white rounded-lg shadow-[0px_4px_12px_0px_rgba(0,0,0,0.10)] outline outline-1 outline-offset-[-1px] outline-slate-200">
+    <nav className="w-[150px] absolute right-0 top-8 z-20 bg-white rounded-lg shadow-[0px_4px_12px_0px_rgba(0,0,0,0.10)] outline-1 outline-offset-[-1px] outline-[#E2E8F0]">
       <ul className="flex flex-col">
         <li>
           <button
             type="button"
-            className="w-full h-11 gap-3 flex items-center justify-center rounded-t-lg hover:bg-[#F1F5F9] active:bg-[#E2E8F0]"
+            className="w-full px-4 py-3 gap-3 flex items-center justify-center rounded-t-[8px] hover:bg-[#F1F5F9] active:bg-[#E2E8F0]"
             onClick={() => router.push('/user')}
           >
             <User width={18} height={18} stroke="#64748B" strokeWidth={1.5} />
-            <span className="w-20 text-[#334155] text-sm font-medium">마이페이지</span>
+            <span className="w-full text-start text-[#334155] text-sm font-medium">마이페이지</span>
           </button>
         </li>
         <li>
           <button
             type="button"
-            className="w-full h-11 gap-3 flex items-center justify-center rounded-b-lg hover:bg-[#F1F5F9] active:bg-[#E2E8F0]"
+            className="w-full px-4 py-3 gap-3 flex items-center justify-center rounded-b-[8px] hover:bg-[#F1F5F9] active:bg-[#E2E8F0]"
             onClick={() => openModal()}
           >
             <Logout width={18} height={18} stroke="#EF4444" strokeWidth={1.5} />
-            <span className="w-20 text-[#EF4444] text-sm font-medium">로그아웃</span>
+            <span className="w-full text-start text-[#EF4444] text-sm font-medium">로그아웃</span>
           </button>
         </li>
       </ul>
