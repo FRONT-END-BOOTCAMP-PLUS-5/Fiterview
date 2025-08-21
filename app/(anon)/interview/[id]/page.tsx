ffe9e19343dc1fbbd886b5df97f8f0777d19ddc5
David Song <david.jh.song@gmail.com>
2025-08-21 16:07:16 +0900

    feat: reflection modal done

--- app/(anon)/interview/[id]/page.tsx
+++ app/(anon)/interview/[id]/page.tsx
@@ -1,11 +1,13 @@
 import InterviewClient from '@/app/(anon)/interview/[id]/components/InterviewClient';
 import CheckInterview from '@/app/(anon)/interview/[id]/components/precheck/CheckInterview';
+import ReflectionModal from '@/app/(anon)/interview/components/modal/ReflectionModal';
 
 export default function Page() {
   return (
     <>
       <CheckInterview />
       <InterviewClient />
+      <ReflectionModal />
     </>
   );
 }
