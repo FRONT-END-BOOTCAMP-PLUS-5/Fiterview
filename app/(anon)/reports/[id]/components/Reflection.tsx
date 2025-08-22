4a2d1f4efe1fbf766cb811d9b48708647314f798
David Song <david.jh.song@gmail.com>
2025-08-22 14:14:33 +0900

    feat: ui css edited

--- app/(anon)/reports/[id]/components/Reflection.tsx
+++ app/(anon)/reports/[id]/components/Reflection.tsx
@@ -53,22 +53,26 @@ export default function Reflection({
   };
 
   return (
-    <div className="w-[450px] min-h-[349px] rounded-xl bg-white border border-slate-200 box-border flex flex-col items-start justify-start p-6 gap-5 text-left text-lg text-slate-800">
-      <div className="self-stretch flex flex-row items-center justify-start gap-2">
+    <div
+      className={`w-[450px] h-[349px] gap-5 rounded-xl bg-white border border-slate-200 box-border flex flex-col items-start justify-start p-6 ${isEditing ? 'pb-4' : 'pb-6'} text-left text-lg text-slate-800`}
+    >
+      <div className="self-stretch flex flex-row items-center justify-start gap-2 mt-[6px]">
         <MessageSquareIcon className="w-5 h-5 relative overflow-hidden flex-shrink-0 text-[#3B82F6]" />
-        <b className="flex-1 relative leading-[21.6px] text-[#1E293B]">면접 회고</b>
+        <b className="flex-1 relative h-[22px] leading-[21.6px] text-[#1E293B]">면접 회고</b>
       </div>
       {isLoading ? (
         <LoadingSpinner />
       ) : (
-        <div className="self-stretch flex flex-col items-start justify-start gap-5 text-sm">
+        <div
+          className={`self-stretch flex flex-col items-start justify-start ${isEditing ? 'gap-4' : 'gap-5'} text-sm mb-[6px]`}
+        >
           <div className="self-stretch flex flex-col items-start justify-start gap-[6px]">
             <div className="self-stretch relative leading-[16.8px] font-medium text-[##374151]">
               면접에서 느낀 생각과 감정을 솔직하게 기록해보세요.
             </div>
 
             <textarea
-              className="w-full min-h-48 p-3 bg-slate-50 rounded-md border border-slate-200 outline-none focus:border-slate-300 text-slate-700 placeholder-slate-400"
+              className="w-full h-47 p-3 bg-slate-50 rounded-md border border-slate-200 outline-none focus:border-slate-300 text-slate-700 placeholder-slate-400"
               value={text}
               onChange={(e) => setText(e.target.value)}
               disabled={!isEditing}
