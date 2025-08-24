b7726b824d632c94c21868ec9c6be7b0f6090642
shinjuhyeon <wngus4595@gmail.com>
2025-08-25 02:53:43 +0900

    feat: 질문을 가져오지 못했을 때 에러 메세지 처리

--- app/(anon)/interview/[id]/components/avatar/Question.tsx
+++ app/(anon)/interview/[id]/components/avatar/Question.tsx
@@ -3,16 +3,19 @@ import MessageCircle from '@/public/assets/icons/message-circle.svg';
 
 interface QuestionProps {
   text: string;
+  isError: boolean;
 }
 
-export default function Question({ text }: QuestionProps) {
+export default function Question({ text, isError }: QuestionProps) {
   return (
     <div className="absolute bottom-[52px] w-[calc(100%-104px)] mx-[52px] p-4 rounded-[8px] bg-white border border-[#E2E8F0] gap-[8px] flex flex-col">
       <div className="flex items-center gap-[8px]">
         <MessageCircle width={16} height={16} stroke="#3B82F6" />
         <p className="text-[12px] text-[#3B82F6] font-semibold">질문</p>
       </div>
-      <p className="text-[14px] leading-[22px] text-[#334155] font-medium">{text}</p>
+      <p className="text-[14px] leading-[22px] text-[#334155] font-medium">
+        {isError ? '질문을 불러오지 못했습니다. 면접을 다시 시작해주세요.' : text}
+      </p>
     </div>
   );
 }
