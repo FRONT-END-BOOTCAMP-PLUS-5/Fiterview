fa71e45ca06f1ebaad76c4dd7b7d8ff85d3a10da
David Song <david.jh.song@gmail.com>
2025-08-27 10:02:38 +0900

    refactor: test code reverted

--- types/report.ts
+++ types/report.ts
@@ -1,6 +1,1 @@
-export const REPORT_STATUS = {
-  PENDING: 'PENDING',
-  ANALYZING: 'ANALYZING',
-  COMPLETED: 'COMPLETED',
-} as const;
-export type ReportStatus = (typeof REPORT_STATUS)[keyof typeof REPORT_STATUS];
+export type ReportStatus = 'PENDING' | 'ANALYZING' | 'COMPLETED';
